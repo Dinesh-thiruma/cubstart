c187a9f8af2774e3772e524de8da203750013de7
Dinesh Thirumavalavan <dineshthiruma@gmail.com>
2022-02-17 10:17:18 -0800

    Finished Part 8

--- hw2/scripts/bored.js
+++ hw2/scripts/bored.js
@@ -3,8 +3,10 @@ const card = document.getElementById("card");
 
 // BEGIN PART 7
 
-async function fetchActivity() {
-  // YOUR CODE HERE
+async function fetchActivity(){
+  const response = await fetch("https://www.boredapi.com/api/activity");
+  const activity = await response.json();
+  return activity;
 }
 
 // END PART 7
