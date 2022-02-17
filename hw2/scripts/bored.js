03967d4668786be1a2241584ec1618b0f1876b7f
Dinesh Thirumavalavan <dineshthiruma@gmail.com>
2022-02-17 12:53:00 -0800

    finished part 9

--- hw2/scripts/bored.js
+++ hw2/scripts/bored.js
@@ -1,5 +1,5 @@
 const boredButton = document.getElementById("bored-button");
-const card = document.getElementById("card");
+const card = document.getElementById("content");
 
 // BEGIN PART 7
 
@@ -17,15 +17,17 @@ boredButton.addEventListener("click", async () => {
   // process this data into 'p' elements
   // append the text elements as children of the card
   const activity = await fetchActivity();
+  const cardDetails = document.getElementById("content")
+  cardDetails.innerHTML = ``;
+  document.getElementById("bored-text").innerHTML = "I'm Still Bored";
   // BEGIN PART 8
   for (const [key, value] of Object.entries(activity)) {
     if(key == "activity") {
       document.getElementById("card-header").innerText = value;
-    }else {
-      // const detail = document.createElement("p");
-      // detail.innerHTML = key + ": " + value;
-      // card.appendChild(detail);
+    }else if (key != "link" && key != "key") {
+      cardDetails.innerHTML += `${key}: ${value}<br>`
     }
   }
+  document.getElementById("column").style="flex";
   // END PART 8
 });
